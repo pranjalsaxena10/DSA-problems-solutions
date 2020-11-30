c3ff7acc46336a1424b3048ee61beabdbc970f91
Pranjal Saxena <pranjalmohansaxena@gmail.com>
2020-11-30 14:52:34 +0530

    Compilation issue

--- LinkedList/Day5_Problems/Merge_Two_Sorted_Linked_Lists/Solution.java
+++ LinkedList/Day5_Problems/Merge_Two_Sorted_Linked_Lists/Solution.java
@@ -8,6 +8,13 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+class ListNode {
+  int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
 
 /**
  * https://leetcode.com/problems/merge-two-sorted-lists/
